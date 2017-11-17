737e35a249ef559d9c589a080308efa4d62f6476
Carlos Stockhausen <c@sugarman.tv>
2017-11-17 21:01:52 +0100

    Hello world

---
+++ src/scripts/components/sample.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name         Trello Skin
+// @namespace    http://carlito.me
+// @version      0.1
+// @description  A skin for Trello.
+// @author       Carlos
+// @match        https://trello.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+  'use strict';
+
+  var cssId = 'TrelloSkin';
+
+  if (!document.getElementById(cssId))
+  {
+    var head  = document.getElementsByTagName('head')[0];
+    var link  = document.createElement('link');
+    link.id   = cssId;
+    link.rel  = 'stylesheet';
+    link.type = 'text/css';
+    link.href = '';
+    link.media = 'all';
+    head.appendChild(link);
+  }
+})();
